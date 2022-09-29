1058618a80d2386c97c0f02f6c54ef884edaed13
ignaciobarbieri <ignaciobarbieri@gmail.com>
2022-09-29 02:21:57 -0300

    I am the Poroelegante  I am the storm

--- GenMundo2D/Assets/Scripts/movimiento.cs
+++ GenMundo2D/Assets/Scripts/movimiento.cs
@@ -24,41 +24,39 @@ public class movimiento : MonoBehaviour
 
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         float movX = Input.GetAxisRaw("Horizontal");
         float movY = Input.GetAxisRaw("Vertical");
         movi = new Vector2(movX, movY).normalized;
-
+   
         PlayerAnimator.SetFloat("Horizontal", movX);
         PlayerAnimator.SetFloat("Vertical", movY);
         PlayerAnimator.SetFloat("Velocidad", movi.magnitude);
 
         
 
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-           if(sigDash > 0){
-                sigDash -= Time.deltaTime;
-                velo = velonormal;
-           }         
-         if(sigDash <= 0){
-            sigDash = limitDash;
-            velo = velo * veloDash;
-         }        
+        if(sigDash > 0){
+            sigDash -= Time.deltaTime;
            
         }
-       
-    
+        if(Input.GetKeyDown(KeyCode.Space) && sigDash <= 0)
+        {
+           sigDash = limitDash;
+           velo = velo * veloDash;
+        }
+     
     }
 
     private void FixedUpdate()
     {
-        
-            jugador.MovePosition(jugador.position + movi * velonormal * Time.fixedDeltaTime);
-              
-        
+        jugador.MovePosition(jugador.position + movi * velo * Time.fixedDeltaTime);     
+             
+            if(sigDash <= 0 && velo >= 25f){
+              velo = 5f;
+              jugador.MovePosition(jugador.position + movi * velo * Time.fixedDeltaTime);      
+            }              
 
     }
 
